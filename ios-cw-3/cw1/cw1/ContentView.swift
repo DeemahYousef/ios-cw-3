2bf9ad36b619c9b6d6ef9d30436fe055b574040f
DeemahYousef <dalkhaledi12345@hotmail.com>
2022-03-06 23:35:42 +0300

    done

---
+++ ios-cw-3/cw1/cw1/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  cw1
+//
+//  Created by DIMA ALKHALEDI on 06/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    
+        ZStack{
+          Image("echo")
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+            Spacer()
+                Text("spirited away")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+                Spacer()
+            
+                SwiftUI.HStack{
+                    Image("no")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 130, height: 130, alignment: .center)
+                }
+                }
+                   
+                    Image("sa")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 130, height: 130, alignment: .center)
+                    }
+                    }
+   
+                    Image("drag")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 130, height: 130, alignment: .center)
+                    Image("hk")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 130, height: 130, alignment: .center)
+                    Image("ok")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 130, height: 130, alignment: .center)
+                }
+                }
+                
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
